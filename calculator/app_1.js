af72f939cf4daccbe70324dc303eeced4c98300a
Dillonmemo <arta1069@gmail.com>
2020-03-07 17:01:55 +0900

    ~ day 3

--- calculator/app_1.js
+++ calculator/app_1.js
@@ -14,18 +14,40 @@ const init = () => {
   for (let i = 0; i < number.length; i++) {
     console.log(number[i]);
     number[i].addEventListener('click', e => {
+      // console.log(e);
       console.log(number[i]);
       let currentString = input.innerHTML;
       let lastChar = currentString[currentString.length - 1];
 
       if (resultDisplayed === false) {
-      } else if (
-        (resultDisplayed === true && lastChar === '+') ||
-        lastChar === '-' ||
-        lastChar === '×' ||
-        lastChar === '÷'
-      ) {
+        // = input.innerHTML = input.innerHTML + e.target.innerHTML;
+        input.innerHTML += e.target.innerHTML;
+      }
+      // else if (
+      //   (resultDisplayed === true && lastChar === '+') ||
+      //   lastChar === '-' ||
+      //   lastChar === '×' ||
+      //   lastChar === '÷'
+      // ) {
+
+      // } else {
+      // }
+    });
+  }
+
+  // 연산자 버튼 클릭 핸들러 생성
+  for (let i = 0; i < operator.length; i++) {
+    operator[i].addEventListener('click', e => {
+      console.log(e.target.innerHTML);
+
+      let currentString = input.innerHTML;
+      let lastChar = currentString[currentString.length - 1];
+
+      if (lastChar === '+' || lastChar === '-' || lastChar === 'x' || lastChar === '÷') {
+        let newString = currentString.substring(0, currentString.length - 1) + e.target.innerHTML;
+      } else if (currentString.length === 0) {
       } else {
+        input.innerHTML += e.target.innerHTML;
       }
     });
   }
