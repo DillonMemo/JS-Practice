aa037ef5772a4871c4d8fca32a337879a7c18f9f
Dillonmemo <arta1069@gmail.com>
2020-03-08 16:32:04 +0900

    day 4.5

--- calculator/app_1.js
+++ calculator/app_1.js
@@ -45,12 +45,56 @@ const init = () => {
 
       if (lastChar === '+' || lastChar === '-' || lastChar === 'x' || lastChar === '÷') {
         let newString = currentString.substring(0, currentString.length - 1) + e.target.innerHTML;
+        input.innerHTML = newString;
       } else if (currentString.length === 0) {
+        alert('숫자가 없습니다.');
       } else {
         input.innerHTML += e.target.innerHTML;
       }
     });
   }
+
+  // 'equal' 버튼 클릭 이벤트 핸들러 생성
+  result.addEventListener('click', e => {
+    let inputString = input.innerHTML;
+
+    let numbers = inputString.split(/\+|\-|\×|\÷/g);
+
+    let operators = inputString.replace(/[0-9]|\./g, '').split('');
+
+    let divide = operators.indexOf('÷');
+    while (divide != -1) {
+      numbers.splice(divide, 2, numbers[divide] / numbers[divide + 1]);
+      operators.splice(divide, 1);
+      divide = operators.indexOf('÷');
+    }
+
+    let multiply = operators.indexOf('×');
+    while (multiply != -1) {
+      numbers.splice(multiply, 2, numbers[multiply] * numbers[multiply + 1]);
+      operators.splice(multiply, 1);
+      multiply = operators.indexOf('×');
+    }
+
+    let subtract = operators.indexOf('-');
+    while (subtract != -1) {
+      numbers.splice(subtract, 2, numbers[subtract] - numbers[subtract + 1]);
+      operators.splice(subtract, 1);
+      subtract = operators.indexOf('-');
+    }
+
+    let add = operators.indexOf('+');
+    while (add != -1) {
+      // using parseFloat is necessary, otherwise it will result in string concatenation :)
+      numbers.splice(add, 2, parseFloat(numbers[add]) + parseFloat(numbers[add + 1]));
+      operators.splice(add, 1);
+      add = operators.indexOf('+');
+    }
+
+    input.innerHTML = numbers[0];
+
+    resultDisplayed = true;
+  });
 };
 
 init();
